56cff0fc8723c26ed74fbda5b350529488e7df3a
Peter Leddiman <peter.leddiman@gmail.com>
2020-09-14 10:39:07 +1000

    Added round caps to arena's lines

--- pacman_game/view/arena_view.py
+++ pacman_game/view/arena_view.py
@@ -1,3 +1,5 @@
+import cairo
+
 class ArenaView:
     def __init__(self, arena):
         self._arena = arena
@@ -10,10 +12,11 @@ class ArenaView:
     def draw(self, cr):
         width = self.rect.width
         height = self.rect.height
+        cr.set_line_cap(cairo.LINE_CAP_ROUND)
         cr.set_source_rgb(0, 0, 0)
         cr.paint()
         cr.set_source_rgb(0.0, 0.0, 1.0)
-        cr.set_line_width(2.0)
+        cr.set_line_width(3)
         cr.move_to(24, 96)
         cr.line_to(width-24, 96)
         cr.line_to(width-24, height-24)
