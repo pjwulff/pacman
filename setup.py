0d29dad59157a331762827623be8b97b1115eb0f
Peter Leddiman <peter.leddiman@gmail.com>
2020-09-14 20:49:02 +1000

    Fixed issue with setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='pacman',
-    version='1.0',
-    description='Pacman',
-    license='GPL',
-    author='Peter Leddiman',
-    author_email='peter.leddiman@gmail.com',
-    packages=['pacman_game'],
-    include_package_data=True,
-    install_requires=['pygobject'],
-    scripts=['pacman']
+    name = 'pacman',
+    version = '1.0',
+    description = 'Pacman',
+    license = 'GPL',
+    author = 'Peter Leddiman',
+    author_email = 'peter.leddiman@gmail.com',
+    packages = [
+        'pacman_game',
+        'pacman_game.controller',
+        'pacman_game.model',
+        'pacman_game.view',
+    ],
+    install_requires = ['pygobject'],
+    scripts = ['pacman']
 )
