806446cf7f71d995dccc4ea7d0c894cbe8042ff8
Peter Leddiman <peter.leddiman@gmail.com>
2020-10-04 11:35:08 +1100

    Added high scores dialogue
    High scores can now be displayed. They are sorted
    and can also be filtered by game settings.

--- pacman_game/controller/high_scores_controller.py
+++ pacman_game/controller/high_scores_controller.py
@@ -15,3 +15,47 @@ class HighScoresController:
             (\"{initials}\", {score}, \"{difficulty}\", \"{shape}\", \"{size}\")""")
         db.commit()
         db.close()
+
+    @classmethod
+    def high_scores(cls, f):
+        db = DB()
+        cursor = db.cursor()
+        filters = []
+
+        if not f["difficulty-easy"]:
+            filters += ["(NOT difficulty=\'easy\')"]
+        if not f["difficulty-medium"]:
+            filters += ["(NOT difficulty=\'medium\')"]
+        if not f["difficulty-hard"]:
+            filters += ["(NOT difficulty=\'hard\')"]
+
+        if not f["shape-square"]:
+            filters += ["(NOT shape=\'square\')"]
+        if not f["shape-hexagonal"]:
+            filters += ["(NOT shape=\'hexagonal\')"]
+        if not f["shape-graph"]:
+            filters += ["(NOT shape=\'graph\')"]
+
+        if not f["size-small"]:
+            filters += ["(NOT size=\'small\')"]
+        if not f["size-medium"]:
+            filters += ["(NOT size=\'medium\')"]
+        if not f["size-large"]:
+            filters += ["(NOT size=\'large\')"]
+
+        f = " AND ".join(filters)
+        if f == "":
+            cursor.execute("""
+            SELECT initials, score
+            FROM high_scores
+            ORDER BY score DESC;""")
+        else:
+            cursor.execute(f"""
+                SELECT initials, score
+                FROM high_scores
+                WHERE {f}
+                ORDER BY score DESC;""")
+        res = cursor.fetchall()
+        db.commit()
+        db.close()
+        return res
